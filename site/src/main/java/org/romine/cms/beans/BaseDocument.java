6ac80ba5906ceb8cd964ebb43e553bc1237412ef
Aaron Romine <aaron.romine@gmail.com>
2015-12-13 19:28:33 -0500

    rewriting history

---
+++ site/src/main/java/org/romine/cms/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.romine.cms.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="RomineOrg:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
---
+++ site/src/main/webapp/WEB-INF/ftl/index.ftl
@@ -0,0 +1,6 @@
+<html>
+	<head></head>
+	<body>
+		<p>This is an initial page, nothing to see here.</p>
+	</body>
+</html>
